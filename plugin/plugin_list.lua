88dc3ac8efd8211f43482744438d1465fac72841
Ethan Domingo <etddomin@ucsc.edu>
2025-08-25 19:10:51 -0700

    adding line for easy reload

--- plugin/plugin_list.lua
+++ plugin/plugin_list.lua
@@ -1,3 +1,5 @@
 vim.api.nvim_create_user_command("PluginList", function()
+	-- easily reload the plugin in case of changes
+	package.loaded["plugin_list"] = nil
 	require("plugin_list").openWindow()
 end, {})
